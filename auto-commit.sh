142828c94e6189d33eb6578c356e475a9d7ddc0e
AbdullahBaig7 <94072112+AbdullahBaig7@users.noreply.github.com>
2025-06-11 02:10:35 -0700

    Auto-commit: June 11, 2025 at 02:10 AM

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+cd /workspaces/My-CS50-Projects
+git add .
+msg="Auto-commit: $(date '+%B %d, %Y at %I:%M %p')"
+git commit -m "$msg"
+git push#!/bin/bash
 msg="Auto-commit on $(date +'%Y-%m-%d %H:%M:%S')"
 git add .
 git commit -m "$msg"
